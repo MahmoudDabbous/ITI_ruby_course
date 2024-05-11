23399f16aae42a331ab2f84251e645799866d911
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-05-11 18:25:02 +0300

    Lab 2

--- app.rb
+++ app.rb
@@ -1,152 +1,120 @@
-class Book
-  attr_accessor :title, :author, :isbn, :count
-  def initialize(title, author, isbn)
-    @title = title
-    @author = author
-    @isbn = isbn
-    @count = 1
-  end
-end
+require 'time'
 
-class Inventory
-  attr_reader :books, :name 
-  def initialize(name)
-    @books = begin
-      if File.exist?("#{name}.txt")
-        File.readlines("#{name}.txt").map do |line|
-          title, author, isbn, count = line.split(", ")
-          book = Book.new(title, author, isbn)
-          book.count = count.to_i
-          book
-        end
-      else
-        File.new("#{name}.txt", "w")
-        []
-      end
-    end
-    @name = name
+module Logger
+  def log_info(message)
+    save_to_log_files("info", message)
   end
 
-  def add_books(*books)
-    books.each do |book|
-      existing_book = @books.find { |b| b.isbn == book.isbn }
-      if existing_book
-        existing_book.title = book.title
-        existing_book.author = book.author
-        existing_book.count += 1
-      else
-        @books << book
-      end
-    end
-    save_to_file
+  def log_warning(message)
+    save_to_log_files("warning", message)
   end
 
-  def list_books
-    @books.each do |book|
-      puts "Title: #{book.title}, Author: #{book.author}, ISBN: #{book.isbn}, Count: #{book.count}"
-    end
-  end
-
-  def remove_book(isbn)
-    @books.each do |book|
-      if book.isbn == isbn
-        @books.delete(book)
-      end
-    end
-    save_to_file
-  end
-
-  def sort_books
-    @books.sort_by! { |book| book.isbn }
-    save_to_file
-  end
-
-  def search_by_title(title)
-    @books.each do |book|
-      if book.title == title
-        puts "Title: #{book.title}, Author: #{book.author}, ISBN: #{book.isbn}, Count: #{book.count}"
-      end
-    end
-  end
-
-  def search_by_author(author)
-    @books.each do |book|
-      if book.author == author
-        puts "Title: #{book.title}, Author: #{book.author}, ISBN: #{book.isbn}, Count: #{book.count}"
-      end
-    end
-  end
-
-  def search_by_isbn(isbn)
-    @books.each do |book|
-      if book.isbn == isbn
-        puts "Title: #{book.title}, Author: #{book.author}, ISBN: #{book.isbn}, Count: #{book.count}"
-      end
-    end
+  def log_error(message)
+    save_to_log_files("error", message)
   end
 
   private
 
-  def save_to_file
-    File.open("#{name}.txt", "w") do |file|
-      @books.each do |book|
-        file.puts "#{book.title}, #{book.author}, #{book.isbn}, #{book.count}"
+  def save_to_log_files(log_type, message)
+    timestamp = Time.now.iso8601
+    File.open("app.log", "a") do |file|
+      file.puts "#{timestamp} -- #{log_type} -- #{message}"
+    end
+  end
+end
+
+class User
+  attr_reader :name, :balance
+
+  def initialize(name, balance)
+    @name = name
+    @balance = balance
+  end
+
+  def decrease_balance(amount)
+    raise "Not enough balance" if @balance < amount
+    @balance -= amount
+  end
+end
+
+class Transaction
+  attr_reader :user, :value
+
+  def initialize(user, value)
+    @user = user
+    @value = value
+  end
+end
+
+class Bank
+  def process_transactions(transactions, &callback)
+    raise "#{self.class} has not implemented method '#{__method__}'"
+  end
+end
+
+class CBABank < Bank
+  include Logger
+
+  def initialize(users)
+    @users = users
+  end
+
+  def process_transactions(transactions, &callback)
+    transactions_list = transactions.map { |t| "#{t.user.name} transaction with value #{t.value}" }
+    log_info("Processing Transactions #{transactions_list.join(", ")}")
+
+    transactions.each do |transaction|
+      begin
+        user = @users.find { |user| user.name == transaction.user.name }
+
+        if user
+          user.decrease_balance(transaction.value.abs)
+          log_info("User #{transaction.user.name} transaction with value #{transaction.value} succeeded")
+
+          if user.balance == 0
+            log_warning("#{user.name} has 0 balance")
+          end
+
+          callback.call("success", transaction) 
+        else
+          raise "#{transaction.user.name} don't exist in the bank."
+        end
+
+      rescue => e
+        log_error("User #{transaction.user.name} transaction with value #{transaction.value} failed with message #{e.message}")
+        callback.call("failure", transaction, e.message) 
       end
     end
   end
 end
 
 if __FILE__ == $0
-  inventory = Inventory.new("inventory")
+  users = [
+    User.new("Ali", 200),
+    User.new("Peter", 500),
+    User.new("Manda", 100)
+  ]
 
-  loop do
-    puts "Menu:"
-    puts "1. List books"
-    puts "2. Add new book"
-    puts "3. Remove book by ISBN"
-    puts "4. Sort books by ISBN"
-    puts "5. Search books by title"
-    puts "6. Search books by author"
-    puts "7. Search books by ISBN"
-    puts "0. Exit"
+  out_side_bank_users = [
+    User.new("Menna", 400)
+  ]
 
-    choice = gets.chomp.to_i
+  transactions = [
+    Transaction.new(users[0], -20),
+    Transaction.new(users[0], -30),
+    Transaction.new(users[0], -50),
+    Transaction.new(users[0], -100),
+    Transaction.new(users[0], -100),
+    Transaction.new(out_side_bank_users[0], -100)
+  ]
 
-    case choice
-      when 1
-        inventory.list_books
-      when 2
-        puts "Enter book title:"
-        title = gets.chomp
-        puts "Enter book author:"
-        author = gets.chomp
-        puts "Enter book ISBN:"
-        isbn = gets.chomp
-        book = Book.new(title, author, isbn)
-        inventory.add_books(book)
-      when 3
-        puts "Enter book ISBN to remove:"
-        isbn = gets.chomp
-        inventory.remove_book(isbn)
-      when 4
-        inventory.sort_books
-      when 5
-        puts "Enter book title to search:"
-        title = gets.chomp
-        inventory.search_by_title(title)
-      when 6
-        puts "Enter book author to search:"
-        author = gets.chomp
-        inventory.search_by_author(author)
-      when 7
-        puts "Enter book ISBN to search:"
-        isbn = gets.chomp
-        inventory.search_by_isbn(isbn)
-      when 0
-        break
-      else
-        puts "Please try again."
+  cba_bank = CBABank.new(users)
+
+  cba_bank.process_transactions(transactions) do |status, transaction, reason = nil|
+    if status == "success"
+      puts "Call endpoint for success of User #{transaction.user.name} transaction with value #{transaction.value}"
+    else
+      puts "Call endpoint for failure of User #{transaction.user.name} transaction with value #{transaction.value} with reason #{reason}"
     end
   end
 end
-
