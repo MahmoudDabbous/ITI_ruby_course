e8c363cb7e79382c5967335cd4c019cc538c57bd
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-05-17 18:51:47 +0300

    Lab 4

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,18 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  before_action :authenticate_user!
+  before_action :set_article, only: [:show, :edit, :update, :destroy, :report]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+    if @article.status == 'public' || @article.user == current_user
+      render :show
+    else
+      redirect_to articles_path, alert: "You are not authorized to view this article"
+    end
   end
 
   def new
@@ -14,6 +21,8 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.image.attach(params[:image])
+    @article.save 
 
     if @article.save
       redirect_to @article
@@ -23,13 +32,11 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
-
     if @article.update(article_params)
+      @article.image.attach(params[:image]).save if params[:image].present?
       redirect_to @article
     else
       render :edit, status: :unprocessable_entity
@@ -37,14 +44,29 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
-
     redirect_to root_path, status: :see_other
+  end
+
+  def report
+    @article.reports_count = @article.reports_count + 1
+    if @article.save
+      redirect_to @article, notice: 'Article has been reported.'
+    else
+      redirect_to @article, alert: 'Article could not be reported.'
+    end
   end
 
   private
     def article_params
-      params.require(:article).permit(:title, :body, :status)
+      params.require(:article).permit(:title, :body, :status, :image).merge(user: current_user)
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def authorize_user!
+      redirect_to articles_path, alert: "You are not authorized to do this article" unless @article.user == current_user
     end
 end
